956b00686989f4ba6b31fc096083069b5396248f
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-12 19:51:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, drivers2){
+    return drivers.filter(
+        function (driver){
+            return driver.toLowerCase() === drivers2.toLowerCase()
+        }
+    )
+}
+
+function fuzzyMatch(drivers, drivers2) {
+    return drivers.filter(
+        function (driver){
+            return driver.toLowerCase().substring(0, drivers2.length)=== drivers2.toLowerCase()
+        }
+    )
+}
+
+function matchName(drivers, driver2){
+    return drivers.filter(
+        function (driver){
+            return driver.name === driver2
+        }
+    )
+}
